3bb3ce8fc9e2fe1800253b163d3b66445235ea52
Sean Combs <seanmxd@gmail.com>
2023-09-28 19:59:02 -0400

    Fix

--- restore-docker-volume.sh
+++ restore-docker-volume.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --rm -v $(basename $1 .tar.gz):/restore-dest -v $(dirname $1):/restore-src ubuntu bash -c "cd /restore-dest && rm -rf ./* && rm -rf ./.* && tar -xvf /restore-src/$(basename $1 .tar.gz).tar.gz"
+docker run --rm -v $(basename $1 .tar.gz):/restore-dest -v $(dirname $1):/restore-src ubuntu bash -c "cd /restore-dest && find . -name . -o -prune -exec rm -rf -- {} + && tar -xvf /restore-src/$(basename $1 .tar.gz).tar.gz"
